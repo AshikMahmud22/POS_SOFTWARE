8bb93f581e1defa6d76436fc6652435bf2752fd7
ashik0401 <193727328+AshikMahmud22@users.noreply.github.com>
2026-05-07 22:27:59 +0600

    remove some unnecessary code

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,8 +1,7 @@
 
 import StatisticsChart from "../../components/ecommerce/StatisticsChart";
 import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/UserTable";
-import DemographicCard from "../../components/ecommerce/EaringsCard";
+
 import Dashboard from "../../components/Dashboard/Dashboard";
 import { TrendingUp } from "lucide-react";
 
@@ -28,14 +27,6 @@ export default function Home() {
 
         <div className="col-span-12">
           <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
         </div>
       </div>
     </>
